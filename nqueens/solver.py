ca9729a56f59d07670b4c5ca3738ad975bf27227
Steve Shanks <steve.c.shanks@gmail.com>
2017-04-15 17:20:04 +0100

    Improve speed of solver

--- nqueens/solver.py
+++ nqueens/solver.py
@@ -1,3 +1,6 @@
+from nqueens.threat import Threat
+
+
 class Solver:
 
     @staticmethod
@@ -11,17 +14,22 @@ class Solver:
         return self._tryToPlaceValidQueen(self._board)
 
     def _tryToPlaceValidQueen(self, solution):
+        threats = solution.getUniqueThreats()
         columns = self._getNonThreatenedList(solution.getThreatenedColumns())
         rows = self._getNonThreatenedList(solution.getThreatenedRows())
         for x in columns:
             for y in rows:
+                if Threat.fromPosition(x, y) in threats:
+                    continue
+
                 solution.placeQueen(x, y)
-                if (solution.isValid()):
-                    if solution.getQueenCount() == solution.getSize():
-                        return solution
-                    newSolution = self._tryToPlaceValidQueen(solution)
-                    if newSolution is not None:
-                        return newSolution
+                if solution.getQueenCount() == solution.getSize():
+                    return solution
+
+                newSolution = self._tryToPlaceValidQueen(solution)
+                if newSolution is not None:
+                    return newSolution
+
                 solution.removeQueen(x, y)
 
         return None
